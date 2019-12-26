6fec01314341a405d2c0f6271bea4ed05662783c
Xavier Millot <xavier@codenectar.com>
2019-12-26 17:25:48 -0500

    Some more

--- src/widgets/Checklist/ChecklistStyles.js
+++ src/widgets/Checklist/ChecklistStyles.js
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+  import styled from "styled-components";
 import {colored} from "../../styles/constants";
 import {Loader} from '../../styles/loader-styles'
 
